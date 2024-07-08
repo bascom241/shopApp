e11d8a7e5b89c9562eb1ccc5c0a0d56527378f6a
bascom241 <135514268+bascom241@users.noreply.github.com>
2024-07-08 22:32:45 +0100

    Update CheckoutPage.js

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/shop-context';
 import { productLists } from '../data';
 import './CheckoutPage.css';
 
+
 const CheckoutPage = () => {
   const { cartItems } = useContext(ShopContext);
   const [userDetails, setUserDetails] = useState({
@@ -39,74 +40,88 @@ const CheckoutPage = () => {
   };
 
   return (
+    <>
+
+
     <div className='checkout-container'>
-      <h1>Checkout</h1>
+
       <form className='user-details-form' onSubmit={handleSubmit}>
-        <h2>Shipping Information</h2>
+        <h2>Personal Details</h2>
         <div className='form-row'>
           <label>
             Full Name:
             <input type='text' name='fullName' value={userDetails.fullName} onChange={handleChange} required />
           </label>
-          <label>
-            Country:
-            <input type='text' name='country' value={userDetails.country} onChange={handleChange} required />
-          </label>
         </div>
         <div className='form-row'>
+
+
           <label>
-            State:
+            Email Address
+            <input type='text' name='country' value={userDetails.country} onChange={handleChange} required />
+          </label>
+          <label>
+            Phone number
+            <input type='tel' name='phone' value={userDetails.phone} onChange={handleChange} required />
+          </label></div>
+
+
+        <h2>Shipping Information</h2>
+        <div className='form-row'>
+          <label>
+            Country
             <input type='text' name='state' value={userDetails.state} onChange={handleChange} required />
           </label>
           <label>
-            Zip Code:
+            State
             <input type='text' name='zipCode' value={userDetails.zipCode} onChange={handleChange} required />
           </label>
         </div>
         <div className='form-row'>
           <label>
-            City:
+            City
             <input type='text' name='city' value={userDetails.city} onChange={handleChange} required />
           </label>
           <label>
-            Address:
+            ZipCode
             <input type='text' name='address' value={userDetails.address} onChange={handleChange} required />
           </label>
         </div>
         <div className='form-row'>
           <label>
-            Email:
+            Address
             <input type='email' name='email' value={userDetails.email} onChange={handleChange} required />
           </label>
-          <label>
-            Phone:
-            <input type='tel' name='phone' value={userDetails.phone} onChange={handleChange} required />
-          </label>
+
         </div>
         <h2>Payment Details</h2>
+        <p>Choose a payment method</p>
         <div className='form-row'>
           <label>
-            Card Number:
+            Name on card
             <input type='text' name='cardNumber' value={userDetails.cardNumber} onChange={handleChange} required />
-          </label>
-          <label>
-            Expiry Date:
-            <input type='text' name='expiryDate' value={userDetails.expiryDate} onChange={handleChange} required />
           </label>
         </div>
         <div className='form-row'>
           <label>
-            CVV:
+            Card Number
+            <input type='number' name='email' value={userDetails.email} onChange={handleChange} required />
+          </label>
+
+        </div>
+        <div className='form-row'>
+          <label>
+            Expiry Date
             <input type='text' name='cvv' value={userDetails.cvv} onChange={handleChange} required />
           </label>
           <label>
-            Name on Card:
+            CVV
             <input type='text' name='nameOnCard' value={userDetails.nameOnCard} onChange={handleChange} required />
           </label>
         </div>
-        <button type='submit' className='checkout-button'>Submit</button>
+       
       </form>
-      <div className='checkout-products'>
+      {/* <div className='checkout-products'>
         {productLists.map((product) => {
           const quantity = cartItems[product.id];
           if (quantity > 0) {
@@ -126,12 +141,54 @@ const CheckoutPage = () => {
           }
           return null;
         })}
+      </div> */}
+     
+
+
+
+
+
+      <div className='cart-container'>
+        <div>
+          <h1>Order Summary</h1>
+        </div>
+        <div className='product-contain'>
+          <div className='product-table'>
+            <div className='product-row header'>
+              <div className='product-cell'>Image</div>
+              <div className='product-cell'>Details</div>
+              <div className='product-cell'>Quantity</div>
+            </div>
+            {productLists.map((product) => {
+              if (cartItems[product.id]) {
+                return (
+                  <div className='product-row' key={product.id}>
+                    <div className='product-cell'><img src={product.image} alt={product.tittle} className='product-image' /></div>
+                    <div className='product-cell image-detail'>
+                      <h4>{product.tittle}</h4>
+                      <p>${product.price}</p>
+                    </div>
+                    <div className='product-cell'>{cartItems[product.id]}</div>
+                  </div>
+                );
+              }
+              return null;
+            })}
+          </div>
+
+        </div>
+
       </div>
-      <div className='checkout-summary'>
-        <h2>Total: ${calculateTotal()}</h2>
-        <button className='checkout-button'>Proceed to Payment</button>
-      </div>
+     
+
     </div>
+
+    <div className='checkout-summary'>
+        <h2>Total: ${calculateTotal()}</h2>
+
+      </div>
+    <button type='submit' className='checkout-button'>Place an order</button>
+    </>
   );
 };
 
